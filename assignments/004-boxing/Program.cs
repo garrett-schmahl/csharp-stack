cc59f3cea83548f2afd4474a3b345920192b6886
Garrett Schmahl <gschmahl@pm.me>
2021-06-07 18:20:06 -0700

    boxing finished

---
+++ assignments/004-boxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace _004_boxing
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<object> bundle = new List<object>();
+      bundle.Add(7);
+      bundle.Add(28);
+      bundle.Add(-1);
+      bundle.Add(true);
+      bundle.Add("chair");
+
+      int bundleSum = 0;
+      foreach (object item in bundle)
+      {
+        Console.WriteLine(item);
+        if (item is int)
+        {
+          bundleSum += (int)item;
+        }
+      }
+      Console.WriteLine(bundleSum);
+    }
+  }
+}
