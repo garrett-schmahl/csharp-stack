184df2dd160b78ad12cad405dd2abd6c9351b109
Garrett Schmahl <gschmahl@pm.me>
2021-06-10 11:17:09 -0700

    multiplication table complete no bonus

---
+++ assignments/013-multiplication-table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _013_multiplication_table
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[,] multiplicationTable = new int[10, 10];
+
+      //creating table
+      for (int i = 0; i < 10; i++)
+      {
+        for (int j = 0; j < 10; j++)
+        {
+          multiplicationTable[i, j] = (i + 1) * (j + 1);
+        }
+      }
+
+    }
+  }
+}
