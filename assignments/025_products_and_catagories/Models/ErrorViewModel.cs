5863591cd9af07b052d7f809943c0d877f12e281
Garrett Schmahl <gschmahl@pm.me>
2021-06-30 09:04:06 -0700

    products and cat started

---
+++ assignments/025_products_and_catagories/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _025_products_and_catagories.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
